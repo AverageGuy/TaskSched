641e8b5c2cead6902bc7b58fe83b2de0ff5c2c6b
AverageGuy <jim@k4gvo.com>
2024-08-08 04:50:27 -0400

    Various changes and updates.

--- examples/DemoTemplate/DemoTemplate.ino
+++ examples/DemoTemplate/DemoTemplate.ino
@@ -1,30 +1,3 @@
-/*
-  -----------------------
-  Demo OTA based on ElegantOTA - Demo Example
-  -----------------------
-
-  Skill Level: Beginner
-
-  This example provides with a bare minimal app with ElegantOTA functionality.
-
-  Github: https://github.com/ayushsharma82/ElegantOTA
-  WiKi: https://docs.elegantota.pro
-
-  Works with following hardware:
-  - ESP8266
-  - ESP32
-  - RP2040 (with WiFi) (Example: Raspberry Pi Pico W)
-
-
-  Important note for RP2040 users:
-  - RP2040 requires LittleFS partition for the OTA updates to work. Without LittleFS partition, OTA updates will fail.
-    Make sure to select Tools > Flash Size > "2MB (Sketch 1MB, FS 1MB)" option.
-  - If using bare RP2040, it requires WiFi module like Pico W for ElegantOTA to work.
-
-  -------------------------------
-
-  Upgrade to ElegantOTA Pro: https://elegantota.pro
-*/
 
 /**
   TemplateDemo
